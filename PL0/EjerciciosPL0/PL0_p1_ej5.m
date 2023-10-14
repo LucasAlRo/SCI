d3aa32ebbb621f9f44f3ba9180cff5d94a5a7f00
Lucas <lucasalvarezrodriguez@gmail.com>
2023-10-14 15:22:40 +0200

    Ej 5 hecho

--- PL0/EjerciciosPL0/PL0_p1_ej5.m
+++ PL0/EjerciciosPL0/PL0_p1_ej5.m
@@ -12,19 +12,39 @@ Además, añada la barra de color al contorno. Deben añadirse etiquetas a
 los ejes, y un título a cada gráfica.
 %}
 
+clear all
+close all
+
 % Crear rejilla uniforme de 20 valores x,y entre 5 y -5
 vector_x=linspace(-5,5,20);
 vector_y=linspace(-5,5,20);
 
-% 
 [X,Y]=meshgrid(vector_x,vector_y);
 
-% .* significa cada elemento de la matriz
+% .* significa cada elemento de la matriz 
 Z = Y.*sin(pi*X/10)+5*cos((X.^2+Y.^2)/8)+cos(X+Y).*cos(3*X-Y);
-figure(1);
+
+%% Plotting
+figure('Name','Diagramas de superficie, malla y contorno','NumberTitle','off');
+
+% Superficie
 subplot(2,2,[1,2])
 surf(X,Y,Z);
+colorbar         % Aporta visibilidad a los plots
+xlabel('X');
+ylabel('Y');
+title('Diagrama de superfricie');
+
+% Malla
 subplot(2,2,3)
 mesh(X,Y,Z);
+xlabel('X');
+ylabel('Y');
+title('Diagrama de malla');
+
+% Contorno
 subplot(2,2,4)
 contourf(X,Y,Z);
+xlabel('X');
+ylabel('Y');
+title('Diagrama de contorno');
