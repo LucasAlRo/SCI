e1b8db2a1d6f569f8c9a01be708630d8d7fea8f8
Lucas <lucasalvarezrodriguez@gmail.com>
2023-10-14 15:14:05 +0200

    Update PL0_p1_ej5.m

--- PL0/EjerciciosPL0/PL0_p1_ej5.m
+++ PL0/EjerciciosPL0/PL0_p1_ej5.m
@@ -10,3 +10,21 @@ la superficie, en la parte inferior izquierda la gráfica de la superficie
 en forma de malla y en la parte inferior derecha la gráfica del contorno. 
 Además, añada la barra de color al contorno. Deben añadirse etiquetas a 
 los ejes, y un título a cada gráfica.
+%}
+
+% Crear rejilla uniforme de 20 valores x,y entre 5 y -5
+vector_x=linspace(-5,5,20);
+vector_y=linspace(-5,5,20);
+
+% 
+[X,Y]=meshgrid(vector_x,vector_y);
+
+% .* significa cada elemento de la matriz
+Z = Y.*sin(pi*X/10)+5*cos((X.^2+Y.^2)/8)+cos(X+Y).*cos(3*X-Y);
+figure(1);
+subplot(2,2,[1,2])
+surf(X,Y,Z);
+subplot(2,2,3)
+mesh(X,Y,Z);
+subplot(2,2,4)
+contourf(X,Y,Z);
